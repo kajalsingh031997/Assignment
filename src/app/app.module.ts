c5ad5670f5d175109df132e26da87a46f20e6072
kajal <kajal.singh@oodlestechnologies.com>
2020-04-15 23:50:59 +0530

    First Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { JobComponent } from './job/job.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomePageComponent } from './home-page/home-page.component';
+// import { NO_ERRORS_SCHEMA } from 'blockly';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobComponent,
+    NavigationComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component:HomePageComponent},
+      {path: 'job', component: JobComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
+  // schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
+
